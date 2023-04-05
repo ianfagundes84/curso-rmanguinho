285ebf28e6265c9423d141622bfbd0cf7ad4eb3b
ianfagundes <ianfag@icloud.com>
2023-04-05 00:24:32 -0300

    improve test name

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -25,7 +25,7 @@ protocol HttpPostClient {
 }
 
 final class RemoteAddAccountTests: XCTestCase {
-    func test_() throws {
+    func test_add_should_call_httpClient_with_correct_url() throws {
         let url = URL(string: "http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
