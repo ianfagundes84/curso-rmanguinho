70024a032cc7572a9f4ca16f60d56e4d3b6f8696
ianfagundes <ianfag@icloud.com>
2023-04-08 01:27:58 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with invalid data

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -39,7 +39,7 @@ final class RemoteAddAccountTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
-    func test_add_should_complete_with_error_if_client_completes_with_data() throws {
+    func test_add_should_complete_with_error_if_client_completes_with_valid_data() throws {
         let (sut, httpClientSpy) = makeSut()
         let exp = expectation(description: "waiting")
         let expectedAccount = makeAccountModel()
@@ -53,6 +53,21 @@ final class RemoteAddAccountTests: XCTestCase {
         httpClientSpy.completeWithData(expectedAccount.toData()!)
         wait(for: [exp], timeout: 1)
     }
+    
+    func test_add_should_complete_with_error_if_client_completes_with_invalid_data() throws {
+        let (sut, httpClientSpy) = makeSut()
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: makeAddAccountModel()) { result in
+            switch result {
+            case .failure(let error): XCTAssertEqual(error, .unexpected)
+            case .success: XCTFail("Expected error receive \(result) instead")
+            }
+            exp.fulfill()
+        }
+        httpClientSpy.completeWithData(Data("invalid_data".utf8))
+        wait(for: [exp], timeout: 1)
+    }
+
 }
 
 extension RemoteAddAccountTests {
