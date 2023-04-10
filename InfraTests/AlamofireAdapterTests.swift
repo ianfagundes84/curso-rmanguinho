84ad979fab2474579b8d3573bb443dee1e9cf4e1
ianfagundes <ianfag@icloud.com>
2023-04-10 00:08:10 -0300

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Ian Fagundes on 10/04/23.
+//
+
+import XCTest
+
+final class AlamofireAdapterTests: XCTestCase {
+    func test_() throws {
+        
+    }
+
+
+}
