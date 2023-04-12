5cca6dedb7d912e0149d2f251daded1ab558df0f
ianfagundes <ianfag@icloud.com>
2023-04-12 19:33:16 -0300

    commit with test failing will be revised by Rodrigo - error request.httpBodyStream is always nil.

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -9,8 +9,14 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(to url: URL, with data: Data?) {
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).resume()
+    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data, HttpError>) -> Void) {
+        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
+            switch dataResponse.result {
+            case .failure:
+                completion(.failure(.noConnectivity))
+            case .success: break
+            }
+        }
     }
 }
 
@@ -26,10 +32,23 @@ class AlamofireAdapterTests: XCTestCase {
     }
     
     func test_post_should_make_request_with_no_data() {
-        let sut = makeSut()
         testRequestFor(data: nil) { request in
             XCTAssertNil(request.httpBodyStream)
         }
+    }
+    
+    func test_post_should_complete_with_error_when_request_completes_with_error() {
+        let sut = makeSut()
+        UrlProtocolStub.simulate(data: nil, response: nil, error: makeError())
+        let exp = expectation(description: "waiting")
+        sut.post(to: makeUrl(), with: makeValidData()) { result in
+            switch result {
+            case .failure(let error): XCTAssertEqual(error, .noConnectivity)
+            case .success: XCTFail("Expected error got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -40,22 +59,31 @@ extension AlamofireAdapterTests {
         let session = Session(configuration: configuration)
         let sut =  AlamofireAdapter(session: session)
         checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
     }
     
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
         let sut = makeSut()
-        sut.post(to:  url, with: data)
         let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
-            action(request)
-            exp.fulfill()
-        }
+        sut.post(to:  url, with: data) { _ in exp.fulfill()}
+        var request: URLRequest?
+        UrlProtocolStub.observeRequest { request = $0 }
         wait(for: [exp], timeout: 1)
+        action(request!)
     }
 }
 
 class UrlProtocolStub: URLProtocol {
     static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var response: HTTPURLResponse?
+    static var error: Error?
+    
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
+    }
     
     static func observeRequest(completion: @escaping (URLRequest) -> Void) {
         UrlProtocolStub.emit = completion
@@ -71,6 +99,16 @@ class UrlProtocolStub: URLProtocol {
     
     override open func startLoading() {
         UrlProtocolStub.emit?(request)
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self)
     }
     
     override open func stopLoading() {}
---
+++ InfraTests/Helpers/TestExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  TestExtensions.swift
+//  InfraTests
+//
+//  Created by Ian Fagundes on 11/04/23.
+//
+
+import Foundation
+import XCTest
+
+extension XCTestCase {
+    func checkMemoryLeak(for instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, file: file, line: line)
+        }
+    }
+}
